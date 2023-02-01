b4a324e10f3e38b067cd8ae337e9e1d5c0471c96
Alexander Ptakhin <me@aptakhin.name>
2023-02-01 22:02:34 +0100

    Okay, given up with opening things

--- 2023-Python-Web-Performance-101/pdf/tests/pdfize.spec.ts
+++ 2023-Python-Web-Performance-101/pdf/tests/pdfize.spec.ts
@@ -1,15 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 
 test('screenshot', async ({ page }) => {
   await page.goto('http://localhost:4004/2023-Python-Web-Performance-101/')
   await new Promise(f => setTimeout(f, 1000));
-  // await expect(page.locator("py-splashscreen")).toHaveCount(0)
   await page.locator("py-splashscreen").isHidden();
   await new Promise(f => setTimeout(f, 1000));
-
-  // await page.pdf({ path: 'page1.pdf' });
-
 
   let pageNumber = 1
 
@@ -23,14 +19,17 @@ test('screenshot', async ({ page }) => {
     pageNumber += 1
 
     const visiblePyTerminals = await page.$$("py-terminal:visible");
-    console.log('DD', pageNumber, 'pyterm', visiblePyTerminals.length)
+    console.log('DD', pageNumber, 'pyterm', visiblePyTerminals.length, visiblePyTerminals[0])
 
     if (visiblePyTerminals.length) {
       const visiblePyTerminal = await page.locator("py-terminal:visible")
-      await visiblePyTerminal.press('Shift+Enter')
-      await new Promise(f => setTimeout(f, 3000))
       await page.pdf({ path: 'out/page' + pageFilenumber + '.pdf' })
       pageNumber += 1
+      // const xx = await visiblePyTerminal.locator('.cm-content')
+      // console.log('F', visiblePyTerminal, visiblePyTerminal.length, xx, xx.length)
+      // await new Promise(f => setTimeout(f, 3000))
+      // await page.pdf({ path: 'out/page' + pageFilenumber + 'aft.pdf' })
+      // pageNumber += 1
     }
 
     const lastMark = await page.$("islast:visible");
